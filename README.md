6b0a43c0be2008e8c44b2b51dcaa927bbbdd2541
Hossein <hosseinmokhtari5555@gmail.com>
2022-01-19 17:50:39 +0330

    added README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Grading
+
+## TODO
+
+* teacher can enter lesson name
+* teacher has the option to reset grades
+* grades are appended not deleted
--- student.py
+++ student.py
@@ -4,6 +4,7 @@ y = open('C:\\Users\\Admin\\PycharmProjects\\grading\\dic.txt', 'r')
 tt = json.load(y)
 print('enter name student')
 a = input()
-print(tt[a])
 if a not in tt:
     print("دانش اموز شما در لیست معلم وجود ندارد ")
+else:
+    print(tt[a])
